52225c440a7028a7afdfdf7ffa240a5339abe026
cdw <785246446@qq.com>
2017-11-20 00:08:59 +0800

    c12

---
+++ fruit2.6/src/com/gem/fruit/pojo/Fruit.java
@@ -0,0 +1,70 @@
+package com.gem.fruit.pojo;
+
+public class Fruit {
+	private int id;
+	private String name;
+	private int price;
+	private int count;
+	private String remark;
+	
+	public Fruit(){}
+	
+	public Fruit(String name, int price, int count, String remark) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.count = count;
+		this.remark = remark;
+	}
+
+	public Fruit(int id, String name, int price, int count, String remark) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.count = count;
+		this.remark = remark;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+	
+}
